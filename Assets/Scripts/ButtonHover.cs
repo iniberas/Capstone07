29f2d24e95ba853f8932aadaae7ef9475a78584c
sam-is-grinding <mungkinchristian@apps.ipb.ac.id>
2026-05-06 20:25:23 +0700

    kelupaan

--- Assets/Scripts/ButtonHover.cs
+++ Assets/Scripts/ButtonHover.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
 
-public class TMPUnderlineToggle : MonoBehaviour
+public class ButtonHover : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
 
